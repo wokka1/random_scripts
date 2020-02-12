611344b72725f69d0fd0dc692b30a50f24ea0a53
wokka1 <wokka@justfamily.org>
2020-02-12 09:49:24 -0600

    Update backup.ps1

--- backup.ps1
+++ backup.ps1
@@ -14,14 +14,24 @@
 # make sure you have a manual backup before running this, just in case you goof it :)
 
 $gameDir = "c:\torch"
-$saveName = "world1"
+$saveName = "beta"
 $backupDir = "c:\tmp"
-$remoteDir = "/remote_dir"
+$remoteDir = "q:\"
 $remoteHost = "192.168.0.20"
+#win share settings
+$passwordLocation = "c:\users\Administrator\Documents\secstr.txt"
+$userName = "Wok"
+
+#scp settings
 $password = ConvertTo-SecureString "myL33tpw" -AsPlainText -Force
 $Cred = New-Object System.Management.Automation.PSCredential ("oberon", $password)
 
 #  don't modify anything below this line
+
+# To set the password : read-host -prompt "Enter password to be encrypted in secstr.txt " -assecurestring | convertfrom-securestring | out-file C:\passFolder\secstr.txt
+$pass = cat $passwordLocation | convertto-securestring
+$mycred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName,$pass
+New-PSDrive -Name "Q" -Root "\\192.168.0.50\2020Wok" -Persist -PSProvider "FileSystem" -Credential $mycred
 
 $timeStamp = $(Get-Date -Format "yyyy-MM-dd-HHmmss")
 
@@ -32,7 +42,7 @@ Compress-Archive -Path $gameDir\instance\saves\$saveName\backup\ -DestinationPat
 Remove-Item -Path $gameDir\instance\saves\$saveName\backup\* -Recurse
 
 # scp the file over to the remote host
-Set-SCPFile -ComputerName $remoteHost -Credential $Cred -Remotepath $remoteDir -LocalFile $backupDir\$saveName-backup-$timeStamp.zip
+# Set-SCPFile -ComputerName $remoteHost -Credential $Cred -Remotepath $remoteDir -LocalFile $backupDir\$saveName-backup-$timeStamp.zip
 
 # copy the backup on network
-# Copy-Item $backupDir\$saveName-backup-$timeStamp.zip $remoteDir
+Copy-Item $backupDir\$saveName-backup-$timeStamp.zip $remoteDir
